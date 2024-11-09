0aed6a799289d7bac65d5cc7ea101dbe38fbb3df
Bhavya Aggarwal <bhavya.aggarwal@gmail.com>
2024-11-09 10:32:49 +0530

    Changed Login Logo

--- ui-ngx/src/app/shared/components/logo.component.ts
+++ ui-ngx/src/app/shared/components/logo.component.ts
@@ -23,10 +23,10 @@ import { Component } from '@angular/core';
 })
 export class LogoComponent {
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/klyff_final_logo.png';
 
   gotoThingsboard(): void {
-    window.open('https://thingsboard.io', '_blank');
+    window.open('https://klyff.com', '_blank');
   }
 
 }
